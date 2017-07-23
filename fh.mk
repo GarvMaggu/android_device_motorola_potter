fd1988af288c8bd15cf17c5c37e35bbcefebe4dd
Garv Maggu <garvmaggu@gmail.com>
2017-07-23 20:35:07 +0530

    FireHound Bringup
    Change-Id: I5c48ac65cf096d0436fa4222b98868d6bbca4331

--- fh.mk
+++ fh.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/motorola/potter/potter-vendor.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/fh/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
@@ -31,7 +31,7 @@ TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := potter
-PRODUCT_NAME := lineage_potter
+PRODUCT_NAME := fh_potter
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
@@ -41,3 +41,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=motorola/potter/potter:7.0/NPN25.137-67/75:user/release-keys \
     PRIVATE_BUILD_DESC="potter-7.0/NPN25.137-67/75:user/release-keys" \
     PRODUCT_NAME="Moto G5 Plus"
+
+export FH_RELEASE=true
